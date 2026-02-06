01fcd1f650cfa1456c66a742105e5a9bc2c59d84
Merlin Hernandez <83794388+MerlinHdz@users.noreply.github.com>
2026-02-06 11:14:05 -0600

    add function to get computer choice

--- rps.js
+++ rps.js
@@ -1,5 +1,33 @@
 // Rock Paper Scissors
 
-for (let i = 0; i < 10; i++) {
-    console.log(i * i);
+// Understand the problem.
+
+// What do I need to make this work?
+
+// Let the code make a random choice between rock, paper, scissors.
+// Prompt the user to enter their choice
+
+// determine if their choice beats the computer's or vice versa.
+
+// display a message saying they won or lost.
+
+
+
+// Create a Function getComputerChoice
+// This function should
+// randonmly return one of the following values:
+// 'rock', 'paper', 'scissors'
+// I will not use an array as it was not covered yet lol
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+
+    if (choice == 0) {
+        return 'rock';
+    } else if (choice == 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
 }
+
+console.log(getComputerChoice());
